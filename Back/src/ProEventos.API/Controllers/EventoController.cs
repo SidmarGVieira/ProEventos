b6c159c2ebd1655668724c6478c5b1fec3daa08c
sidmargvieira <sidmargvieira@gmail.com>
2021-05-30 23:08:42 -0300

    Secão 18 DataContext Migrations

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProEventos.API.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
@@ -12,49 +13,50 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public IEnumerable<Evento> _evento = new Evento[]{
-            new Evento()
-            {
-                EventoId = 1,
-                Tema = "Angular 11 .Net 5",
-                Local= "Londrina",
-                Lote = "1º lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "Foto.png"
-            },
-            new Evento()
-            {
-                EventoId = 2,
-                Tema = "Aula de Angular 11 .Net 5",
-                Local= "Em Sorriso",
-                Lote = "2º lote",
-                QtdPessoas = 235,
-                DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
-                ImagemURL = "Foto2.png"
-            }
-        };
-        public EventoController()
+        private readonly DataContext _context;
+        // public IEnumerable<Evento> _evento = new Evento[]{
+        //     new Evento()
+        //     {
+        //         EventoId = 1,
+        //         Tema = "Angular 11 .Net 5",
+        //         Local= "Londrina",
+        //         Lote = "1º lote",
+        //         QtdPessoas = 250,
+        //         DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
+        //         ImagemURL = "Foto.png"
+        //     },
+        //     new Evento()
+        //     {
+        //         EventoId = 2,
+        //         Tema = "Aula de Angular 11 .Net 5",
+        //         Local= "Em Sorriso",
+        //         Lote = "2º lote",
+        //         QtdPessoas = 235,
+        //         DataEvento = DateTime.Now.AddDays(5).ToString("dd/MM/yyyy"),
+        //         ImagemURL = "Foto2.png"
+        //     }
+        // };
+        public EventoController(DataContext context)
         {
-            
+            this._context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-             return _evento;
+            return _context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
+        public Evento GetById(int id)
         {
-             return _evento.Where(evento => evento.EventoId == id);
+            return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
         }
 
         [HttpPost]
         public string Inserir()
         {
-             return "Exemplo de post";
+            return "Exemplo de post";
         }
 
     }
